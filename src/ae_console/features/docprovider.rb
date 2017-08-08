907842840dba56028f462c2f50b8cbd238864248
Aerilius <aerilius@gmail.com>
2017-08-08 19:31:17 +0200

    Add indexes to docprovider to avoid linear search for prefix matches

--- src/ae_console/features/docprovider.rb
+++ src/ae_console/features/docprovider.rb
@@ -12,6 +12,15 @@ module AE
 
       # [Hash<String,Hash>] docpath => doc_info
       @apis ||= {}
+      # Direct lookup by a token
+      # token => Array<doc_infos>
+      @hash_by_token ||= {}
+      # Ordered for binary any range searches
+      # Array<doc_infos>
+      @index_by_token = []
+      # Ordered for binary any range searches
+      # Array<doc_infos>
+      @index_by_docpath = []
 
       # Loads API docs found in path
       # @param filepaths [String] one or more files to load.
@@ -103,7 +112,21 @@ module AE
       # @param docpath [String] A prefix of a doc path
       # @return [Array<Hash>]
       def self.get_infos_for_docpath_prefix(docpath)
-        return @apis.keys.select{ |key| key.to_s.index(docpath) == 0 }.map{ |key| @apis[key] }
+        return BinarySearch.search_by_prefix(@index_by_docpath, docpath){ |doc_info| doc_info[:path] }
+      end
+
+      # Returns API infos for a given token for all classes that provide this token.
+      # @param token [String] A token
+      # @return [Array<Hash>]
+      def self.get_infos_for_token(token)
+        return @hash_by_token[token] || []
+      end
+
+      # Given a prefix of a token this method returns all API infos for matching tokens.
+      # @param token [String] A token
+      # @return [Array<Hash>]
+      def self.get_infos_for_token_prefix(prefix)
+        return BinarySearch.search_by_prefix(@index_by_token, prefix){ |doc_info| doc_info[:name] }
       end
 
       # @param doc_info [Hash] A string of type declarations as parsed by yardoc.
@@ -141,6 +164,23 @@ module AE
       class << self
 
         private
+
+        def initialize_hash_by_token(api)
+          api.each{ |docpath, doc_info|
+            token = doc_info[:name]
+            @hash_by_token[token] ||= []
+            # This inserts a reference to the same hash, data is not duplicated.
+            @hash_by_token[token] << doc_info
+          }
+        end
+
+        def initialize_index_by_token()
+          @index_by_token = @apis.values.sort_by{ |doc_info| doc_info[:name] }
+        end
+
+        def initialize_index_by_docpath()
+          @index_by_docpath = @apis.values.sort_by{ |doc_info| doc_info[:path] }
+        end
 
         def get_documentation_url_sketchup(classification)
           host = 'http://ruby.sketchup.com/'
@@ -219,6 +259,88 @@ module AE
       end # class << self
 
       class DocNotFoundError < StandardError; end
+
+      module BinarySearch
+
+        # Returns the first item that matches exactly the searched value.
+        # @param array [Array]   The sorted array to search through
+        # @yieldparam  [Object]  An optional attribute getter that returns the string property to compare. 
+        #                        If not provided, items from the array are directly compared.
+        # @yieldreturn  [String]
+        # @return [Object]       The first item in the array that fulfills the comparison.
+        def self.search_exact(array, value, &get_attribute)
+          if block_given?
+            index = binary_search_lowest_index(array){ |item| value <= get_attribute.call(item) }
+            greater_or_equal = array[index]
+            return (value == get_attribute.call(greater_or_equal)) ? greater_or_equal : nil
+          else
+            # Equal to `array.include?(value) ? value : nil`
+            index = binary_search_lowest_index(array){ |item| value <= item }
+            greater_or_equal = array[index]
+            return (value == greater_or_equal) ? greater_or_equal : nil
+          end
+        end
+
+        # @param array  [Array<Object>] The sorted array to search through
+        # @param prefix [String] The string prefix for which all matches should be found
+        # @yieldparam   [Object] An optional attribute getter that returns the string property to compare. 
+        #                        If not provided, items from the array are directly compared.
+        # @yieldreturn  [String]
+        # @return [Array<Object>] An array containing the range of items that match the prefix.
+        def self.search_by_prefix(array, prefix, &get_attribute)
+          raise ArgumentError.new("Prefix must have at least one character") if prefix.empty?
+          # Prefix is the lowest prefix for which all greater or equal items are desired.
+          # The lowest prefix for which all greater or equal items are not desired
+          prefix_succ = prefix[0...-1] + (prefix[-1].ord+1).chr
+          if block_given?
+            first_index      = binary_search_lowest_index(array){ |item| prefix      <= get_attribute.call(item) }
+            after_last_index = binary_search_lowest_index(array){ |item| prefix_succ <= get_attribute.call(item) }
+          else
+            first_index      = binary_search_lowest_index(array){ |item| prefix      <= item }
+            after_last_index = binary_search_lowest_index(array){ |item| prefix_succ <= item }
+          end
+          if first_index < after_last_index
+            return array[first_index...after_last_index]
+          else
+            return []
+          end
+        end
+
+        class << self
+
+          private
+
+          # @param array [Array]      The sorted array to search through
+          # @yieldparam  [Comparable] The block is given the item to compare.
+          # @yieldreturn [Boolean]    The block returns true if the searched item is less or equal the given one.
+          # @return [Integer]         The lowest index that fulfills the comparison, or the length of the array
+          def binary_search_lowest_index(array, &comparison)
+            return binary_search_lowest_index_impl(array, 0, array.length, &comparison)
+          end
+
+          # @param array [Array]   The sorted array to search through
+          # @param first [Integer] The lower index to include
+          # @param last  [Integer] The upper index not to include anymore
+          # @yieldparam  [Comparable]
+          # @yieldreturn [Boolean]
+          # @return [Integer]      The lowest index that fulfills the comparison, or the upper limit of the search.
+          def binary_search_lowest_index_impl(array, first, last, &comparison)
+            # Empty range, not found
+            return first-1 if first == last
+            # One item
+            return comparison.call(array[first]) ? first : last if first == last-1
+            # Multiple items, split by a pivot and recurse.
+            pivot = first + ( (last - first) / 2 )
+            if comparison.call(array[pivot])
+              return binary_search_lowest_index_impl(array, first, pivot, &comparison)
+            else
+              return binary_search_lowest_index_impl(array, pivot, last,  &comparison)
+            end
+          end
+
+        end
+
+      end
 
       DocProvider.load_apis(Dir.glob(File.join(API_PATH, '*.json')))
 

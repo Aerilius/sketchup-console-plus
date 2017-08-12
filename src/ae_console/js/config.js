bd2ceec5cc3e353566c0102ebe9f182be2eef6d9
Aerilius <aerilius@gmail.com>
2017-08-12 15:24:06 +0200

    Move require.js configuration into separate file.

---
+++ src/ae_console/js/config.js
@@ -0,0 +1,31 @@
+// Before loading requirejs.
+var requirejs = {
+    paths: {
+        ace: '../external/ace',
+        bootstrap: '../external/bootstrap/js/bootstrap',
+        'bootstrap-notify': '../external/bootstrap-notify/bootstrap-notify',
+        'bootstrap-filterlist': '../external/bootstrap-typeahead/bootstrap-filterlist',
+        'bootstrap-typeahead': '../external/bootstrap-typeahead/bootstrap-typeahead',
+        features: '../features',
+        jquery: '../external/jquery/jquery'
+    },
+    // Declare dependencies of legacy libraries that don't use define / requirejs.
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: '$'
+        },
+        'bootstrap-notify': {
+            deps: ['jquery'],
+            exports: 'jQuery.notify'
+        },
+        'bootstrap-filterlist': {
+            deps: ['jquery', 'bootstrap-typeahead'],
+            exports: 'jQuery.fn.filterlist'
+        },
+        'bootstrap-typeahead': {
+            deps: ['jquery'],
+            exports: 'jQuery.fn.typeahead'
+        }
+    }
+};
--- src/ae_console/js/main.js
+++ src/ae_console/js/main.js
@@ -1,33 +1,3 @@
-requirejs.config({
-    paths: {
-        ace: '../external/ace',
-        bootstrap: '../external/bootstrap/js/bootstrap',
-        'bootstrap-notify': '../external/bootstrap-notify/bootstrap-notify',
-        'bootstrap-filterlist': '../external/bootstrap-typeahead/bootstrap-filterlist',
-        'bootstrap-typeahead': '../external/bootstrap-typeahead/bootstrap-typeahead',
-        jquery: '../external/jquery/jquery',
-    },
-    // Declare dependencies of legacy libraries that don't use define / requirejs.
-    shim: {
-        bootstrap: {
-            deps: ['jquery'],
-            exports: '$'
-        },
-        'bootstrap-notify': {
-            deps: ['jquery'],
-            exports: 'jQuery.notify'
-        },
-        'bootstrap-filterlist': {
-            deps: ['jquery', 'bootstrap-typeahead'],
-            exports: 'jQuery.fn.filterlist'
-        },
-        'bootstrap-typeahead': {
-            deps: ['jquery'],
-            exports: 'jQuery.fn.typeahead'
-        }
-    }
-});
-
 // Main function
 requirejs(['app', 'bridge', 'console_api', 'jquery'], function (app, Bridge, API, $) {
     // 1. Load the app and UI (done)
@@ -65,5 +35,3 @@ requirejs(['app', 'bridge', 'console_api', 'jquery'], function (app, Bridge, API
         return true;
     };
 });
-
-

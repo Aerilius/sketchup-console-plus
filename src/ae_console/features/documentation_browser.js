728a222687f5a40e702c60d2f120ab6c007d7fee
Aerilius <aerilius@gmail.com>
2017-08-12 19:23:17 +0200

    Add browsing capabilities to documentation browser
    This gives the user more control of the documentation browser when
    lookup failed and gives more possibilities to lookup at other sources.
    Solves issue #1.

---
+++ src/ae_console/features/documentation_browser.js
@@ -0,0 +1,149 @@
+define(['jquery', 'features/history_manager', 'bridge', 'settings', 'console_api'], function ($, History, Bridge, Settings, API) {
+
+    var rubyVersion = '2.0.0';
+    //Bridge.get('ruby_version').then(function (actual) { rubyVersion = actual; });
+    var history = new History();
+    var settings = new Settings();
+
+    function navigateTo (address) {
+        history.registerItem(address);
+        setUrl(address);
+        autoHideNavigation();
+    }
+
+    function setUrl (address) {
+        $('#content_frame').attr('src', address);
+        $('#navigation_address').val(address);
+    }
+
+    function showErrorPage () {
+        setUrl('./documentation_error_page.html'); // Don't add to history.
+        showNavigation();
+    }
+
+    function showNavigation () {
+        $('#navigation_wrapper').css('height', 'initial');
+    }
+
+    function autoHideNavigation () {
+        $('#navigation_wrapper').css('height', '');
+    }
+
+    $(function () { // on DOMContentLoaded
+
+        $('#navigation_backward').on('click', function () {
+            if (history.canUndo()) {
+                var address = history.undo();
+                setUrl(address);
+            }
+        });
+
+        $('#navigation_forward').on('click', function () {
+            if (history.canRedo()) {
+                var address = history.redo();
+                setUrl(address);
+            }
+        });
+
+        $('#navigation_address').on('keyup', function (event) {
+            if (event.which == 13) { /* enter */
+                var input, address;
+                input = $(this).val();
+                if (/^https?\:\/\/[a-zA-Z0-9\-\.]{3,}\.[a-zA-Z]{2,}/.test(input)) {
+                    address = input;
+                } else if (/^[a-zA-Z0-9\-\.]{3,}\.[a-zA-Z]{2,}/.test(input)) {
+                    address = 'http://' + input;
+                } else {
+                    address = $('#search_engine').val().replace(/QUERY/, encodeURIComponent(input));
+                }
+                navigateTo(address);
+            }
+        });
+
+        $('#navigation .bookmark').each(function (index, button) {
+            $(button).on('click', function () {
+                navigateTo($(this).attr('data-href'));
+            });
+        });
+
+        Bridge.call('loaded');
+
+        /*Bridge.get('get_settings').then(function (settings) {
+            var Property = requirejs('lib/property');
+            var searchEngineProperty = new Property('search_engine', settings['search_engine']);
+            $('#search_engine').val(searchEngineProperty.getValue());
+            $('#search_engine').on('change', function () {
+                searchEngineProperty.setValue($(this).val());
+            });
+        });*/
+        Bridge.get('get_settings').then(settings.load);
+        settings.getProperty('search_engine', null).addListener('change', function (newValue) {
+            searchEngineProperty.setValue($(this).val());
+        });
+
+    });
+
+    // Catch uncaught errors in the WebDialog and send them to the console.
+    // There are some errors by the ACE editor (when double-clicking or selection
+    // that goes outside the editor) that would be silent in a normal Internet Explorer
+    // but cause popups in SketchUp.
+    window.onerror = function(messageOrEvent, source, lineNumber, columnNumber, errorObject) {
+        window.console.log([messageOrEvent, source, lineNumber, columnNumber, errorObject]);
+        if (!errorObject) {
+            errorObject = new Error();
+            errorObject.name = 'Error';
+            errorObject.message = messageOrEvent;
+            errorObject.fileName = source;
+            errorObject.lineNumber = lineNumber;
+            errorObject.columnNumber = columnNumber;
+        }
+        API.javaScriptError(errorObject);
+        Bridge.puts('JavaScript Error: '+errorObject.message+'('+source+':'+lineNumber+':'+columnNumber+')');
+        return true;
+    };// TODO: remove
+
+    return {
+        navigateTo: navigateTo,
+        showErrorPage: showErrorPage
+    };
+});
+
+define('features/history_manager', [], function () {
+    /**
+     * History Manager
+     * Cross-origin policy blocks history access in frames, like:
+     *   frame.history.back();
+     * Because of this we implement our own history.
+     */
+    return function () {
+        var before = [];
+        var current = null;
+        var after = [];
+
+        this.registerItem = function (item) {
+            if (after.length > 0) after.length = 0; // clear
+            if (current) before.push(current);
+            current = item;
+        };
+
+        this.canUndo = function () {
+            return before.length > 0;
+        };
+
+        this.undo = function () {
+            if (current) after.push(current);
+            current = before.pop();
+            return current;
+        };
+
+        this.canRedo = function () {
+            return after.length > 0;
+        }
+
+        this.redo = function () {
+            if (current) before.push(current);
+            current = after.pop();
+            return current;
+        };
+    }
+});
--- src/ae_console/features/feature_documentation.rb
+++ src/ae_console/features/feature_documentation.rb
@@ -9,10 +9,12 @@ module AE
       require(File.join(PATH, 'bridge.rb'))
       require(File.join(PATH, 'translate.rb'))
 
+      DOCUMENTATION_BROWSER_HTML ||= File.join(PATH, 'html', 'documentation.html')
       ERROR_PAGE_URL ||= File.join(PATH, 'html', 'docbrowser_error_page.html')
 
       def initialize(app)
         @documentation_browser = nil
+        @settings = app.settings
         app.plugin.on(:console_added, &method(:initialize_console))
       end
 
@@ -35,20 +37,52 @@ module AE
           width = [w, 500].min
           left = x + w # right of console dialog
           left = x - w if left > screen_w || screen_w - left < x # left of console dialog
-          # Show the documentation browser.
+          # Initialize the documentation browser and return a promise that is resolved once the html is loaded.
           if @documentation_browser.nil?
-            @documentation_browser = UI::HtmlDialog.new()
-            Bridge.decorate(@documentation_browser)
-            @documentation_browser.on('translate'){ |action_context|
-              TRANSLATE.webdialog(@documentation_browser)
+            initialize_documentation_browser
+            promise = Bridge::Promise.new
+            @documentation_browser.on('loaded'){ |action_context|
+              promise.resolve(true)
             }
-          end
-          if !@documentation_browser.visible?
             @documentation_browser.set_position(left, y)
             @documentation_browser.set_size(width, h)
+            @documentation_browser.show
+            next promise
+          # Otherwise just show the documentation browser.
+          elsif !@documentation_browser.visible?
+            @documentation_browser.set_position(left, y)
+            @documentation_browser.set_size(width, h)
+            @documentation_browser.show
+          else
+            @documentation_browser.bring_to_front
           end
-          @documentation_browser.show
-          @documentation_browser.bring_to_front
+          next true
+        }
+      end
+
+      def initialize_documentation_browser
+        @documentation_browser = UI::HtmlDialog.new({
+            :dialog_title    => TRANSLATE['Documentation'],
+            :scrollable      => false,
+            :resizable       => true,
+            :style => UI::HtmlDialog::STYLE_DIALOG
+        })
+        @documentation_browser.set_file(DOCUMENTATION_BROWSER_HTML)
+
+        # Add a Bridge to handle JavaScript-Ruby communication.
+        @documentation_browser = Bridge.decorate(@documentation_browser)
+
+        @documentation_browser.on('translate') { |action_context|
+          # Translate.
+          TRANSLATE.webdialog(@documentation_browser)
+        }
+
+        @documentation_browser.on('get_settings') { |action_context| 
+          action_context.resolve @settings.to_hash
+        }
+
+        @documentation_browser.on('update_property') { |action_context, key, value|
+          @settings[key] = value
         }
       end
 
@@ -59,22 +93,22 @@ module AE
         classification = TokenResolver.resolve_tokens(tokens, binding) # raises ResolverError
         url = DocProvider.get_documentation_url(classification)
         if !defined?(Sketchup::Http)
-          @documentation_browser.set_url(url)
+          @documentation_browser.execute_script("requirejs('features/documentation_browser').navigateTo(#{JSON.generate([url])[1...-1]});")
         else
           # Test whether url exists (Sketchup 2017+)
           Sketchup::Http::Request.new(url, Sketchup::Http::HEAD).start{ |request, response|
             if response.status_code == 200
-              @documentation_browser.set_url(url)
+              @documentation_browser.execute_script("requirejs('features/documentation_browser').navigateTo(#{JSON.generate([url])[1...-1]});")
             else
-              @documentation_browser.set_file(ERROR_PAGE_URL) # TODO: or base url of documentation
-              warn("Documentation not found for '#{classification.docpath}' at url #{url}") # TODO: use notification instead
+              error_message = "Documentation not found for #{classification.docpath} at url #{url}"
+              @documentation_browser.execute_script("requirejs('features/documentation_browser').showErrorPage('#{error_message}');")
             end
           }
         end
       rescue TokenResolver::TokenResolverError => error
         # Load an error page
-        @documentation_browser.set_url(ERROR_PAGE_URL)
-        warn("Documentation could not be resolved for '#{tokens.join}'") # TODO: use notification instead
+        error_message = "Documentation could not be resolved for #{JSON.generate([tokens.join])[1...-1]}"
+        @documentation_browser.execute_script("requirejs('features/documentation_browser').showErrorPage('#{error_message}');")
       end
 
       def get_javascript_string

71f31b504d8639fb4761b8d15a26f03646924242
Aerilius <aerilius@gmail.com>
2017-10-08 20:50:24 +0200

    Refactor feature_highlight_entity

--- src/ae_console/features/feature_highlight_entity.rb
+++ src/ae_console/features/feature_highlight_entity.rb
@@ -8,24 +8,20 @@ module AE
 
       def initialize(app)
         app.plugin.on(:console_added, &method(:initialize_console))
+        @is_active = false
+        @instance = nil
+        @model = nil # Reference used while highlighting is active
       end
 
       def initialize_console(console)
         dialog = console.dialog
-        is_active = false
-        instance = nil
-        model = nil # Reference used while highlighting is active
 
         dialog.on('highlight_entity') { |action_context, id_string|
           begin
             object = ObjectSpace._id2ref(id_string.to_i(16) >> 1)
-            model = (object.respond_to?(:model)) ? object.model || Sketchup.active_model : Sketchup.active_model
-            unless is_active
-              instance ||= EntityHighlightTool.new
-              model.tools.push_tool(instance)
-            end
-            instance.highlight(object)
-            is_active = true
+            select_highlighter_tool{ |tool|
+              tool.highlight(entity)
+            }
             # RangeError if no entity found for given id,
             # TypeError "Reference to deleted entity" if entity has been deleted.
           rescue RangeError, TypeError => e
@@ -47,33 +43,39 @@ module AE
             array_xyz.map! { |c| c.mm }
           end
           point = Geom::Point3d.new(array_xyz)
-          model = Sketchup.active_model
-          unless is_active
-            instance ||= EntityHighlightTool.new
-            model.tools.push_tool(instance)
-          end
-          instance.highlight(point)
-          is_active = true
+          select_highlighter_tool{ |tool|
+            tool.highlight(point)
+          }
         }
 
         dialog.on('highlight_vector') { |action_context, array_xyz|
           vector = Geom::Vector3d.new(array_xyz)
-          model = Sketchup.active_model
-          unless is_active
-            instance ||= EntityHighlightTool.new
-            model.tools.push_tool(instance)
-          end
-          instance.highlight(vector)
-          is_active = true
+          select_highlighter_tool{ |tool|
+            tool.highlight(vector)
+          }
         }
 
         dialog.on('highlight_stop') {
-          if is_active && model
-            model.tools.pop_tool
-            model = nil
-            is_active = false
-          end
+          deselect_highlighter_tool()
         }
+      end
+
+      def select_highlighter_tool(&action)
+        unless @is_active
+          @model ||= Sketchup.active_model
+          @instance ||= EntityHighlightTool.new
+          @model.tools.push_tool(@instance)
+        end
+        action.call(@instance) if block_given?
+        @is_active = true
+      end
+
+      def deselect_highlighter_tool
+        if @is_active && @model
+          @model.tools.pop_tool
+          @model = nil
+          @is_active = false
+        end
       end
 
       def get_javascript_path

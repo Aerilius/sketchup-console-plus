7a7e212b74e8c0ecf3aa5ed122d654abd4d93fa8
Aerilius <aerilius@gmail.com>
2018-07-22 14:30:28 +0200

    Refactor long switch cases into methods.

--- src/ae_console/features/feature_highlight_entity.js
+++ src/ae_console/features/feature_highlight_entity.js
@@ -36,97 +36,113 @@ requirejs(['app', 'bridge', 'ace/ace'], function (app, Bridge, ace) {
         $(htmlElement).find('.ace_sketchup').each(function(index, element) {
             var $element = $(element);
             var text = $element.text();
-
             if (regexpEntity.test(text) || regexpBoundingBox.test(text)) {
-                // Add highlight feature to SketchUp entities and get their id.
-                var idString = RegExp.$1;
-                $element.addClass(className)
-                .data('type', 'entity')
-                .data('identifier', idString)
-                .on('mouseover', function() { // Note: This can trigger repeatedly!
-                    Bridge.get('highlight_entity', idString)['catch'](function () {
-                        // If the entity isn't valid (deleted or GC), remove the highlight feature.
-                        $element.removeClass(className);
-                        $element.off('mouseover');
-                        $element.off('mouseout');
-                    });
-                })
-                .on('mouseout', stop);
-
+                addEntityHighlight($element, RegExp);
             } else if (regexpPoint.test(text)) {
-                // Add highlight feature to Point3d (without units: units = 'inch')
-                var coordinates = [parseFloat(RegExp.$1), parseFloat(RegExp.$2), parseFloat(RegExp.$3)];
-                $element.addClass(className)
-                .data('type', 'point')
-                .data('identifier', [coordinates, 'inch'])
-                .on('mouseover', function() {
-                    Bridge.call('highlight_point', coordinates);
-                })
-                .on('mouseout', stop);
-
+                addPointWithoutUnitsHighlight($element, RegExp);
             } else if (regexpPointString.test(text)) {
-                // Add highlight feature to Point3d string
-                var coordinates = $([RegExp.$1, RegExp.$2, RegExp.$3]).map(function(coordinate){
-                    return parseFloat(coordinate.replace(/\,(?=[\d])/, '.'));
-                });
-                var units = RegExp.$4;
-                // Replace unit names so they don't need escaping.
-                switch (units) {
-                    case '"':
-                        units = 'inch';
-                        break;
-                    case "'":
-                        units = 'feet';
-                        break;
-                }
-                $element.addClass(className)
-                .data('type', 'point')
-                .data('identifier', [coordinates, units])
-                .on('mouseover', function() {
-                    Bridge.call('highlight_point', coordinates, units);
-                })
-                .on('mouseout', stop);
-
+                addPointWithUnitsHighlight($element, RegExp);
             } else if (regexpVector.test(text) || regexpVectorString.test(text)) {
-                // Add highlight feature to Vector3d or Vector3d string
-                var coordinates = [parseFloat(RegExp.$1), parseFloat(RegExp.$2), parseFloat(RegExp.$3)];
-                $element.addClass(className)
-                .data('type', 'vector')
-                .data('identifier', coordinates)
-                .on('mouseover', function() {
-                    Bridge.call('highlight_vector', coordinates);
-                })
-                .on('mouseout', stop);
-
+                addVectorHighlight($element, RegExp);
             } else if (regexpColor.test(text)) {
-                // Add highlight feature to Color (only HTML/JS) 
-                // when color has been printed numeral color values
-                var colorString = 'rgb(' + RegExp.$1 + ')';
-                $element.addClass(className)
-                .on('mouseover', function() {
-                    $(this).css('background-color', colorString);
-                }).on('mouseout', function() {
-                    $(this).css('background-color', 'transparent');
-                });
+                addColorHighlight($element, RegExp);
             } else if (regexpColorInspect.test(text)) {
-                // Add highlight feature to Color (only HTML/JS)
-                // when color has been printed as Ruby object with id
-                var idString = RegExp.$1;
-                Bridge.get('get_color', idString).then(function (colorArray) {
-                  var colorString = 'rgb(' + colorArray.slice(0, 3).join(',') + ')';
-                  $element.addClass(className)
-                  .on('mouseover', function() {
-                      $(this).css('background-color', colorString);
-                  }).on('mouseout', function() {
-                      $(this).css('background-color', 'transparent');
-                  });
-                }, function () {
-                    // If the entity isn't valid (deleted or GC), remove the highlight feature.
-                    $element.removeClass(className);
-                    $element.off('mouseover');
-                    $element.off('mouseout');
-                });
+                addColorInspectHighlight($element, RegExp);
             }
+        });
+    }
+
+    function addEntityHighlight($element, regExp) {
+        // Add highlight feature to SketchUp entities and get their id.
+        var idString = regExp.$1;
+        $element.addClass(className)
+        .data('type', 'entity')
+        .data('identifier', idString)
+        .on('mouseover', function() { // Note: This can trigger repeatedly!
+            Bridge.get('highlight_entity', idString)['catch'](function () {
+                // If the entity isn't valid (deleted or GC), remove the highlight feature.
+                $element.removeClass(className);
+                $element.off('mouseover');
+                $element.off('mouseout');
+            });
+        })
+        .on('mouseout', stop);
+    }
+
+    function addPointWithoutUnitsHighlight($element, regExp) {
+        // Add highlight feature to Point3d (without units: units = 'inch')
+        var coordinates = [parseFloat(regExp.$1), parseFloat(regExp.$2), parseFloat(regExp.$3)];
+        $element.addClass(className)
+        .data('type', 'point')
+        .data('identifier', [coordinates, 'inch'])
+        .on('mouseover', function() {
+            Bridge.call('highlight_point', coordinates);
+        })
+        .on('mouseout', stop);
+    }
+
+    function addPointWithUnitsHighlight($element, regExp) {
+        // Add highlight feature to Point3d string with units
+        var coordinates = $([regExp.$1, regExp.$2, regExp.$3]).map(function(coordinate){
+            return parseFloat(coordinate.replace(/\,(?=[\d])/, '.'));
+        });
+        var units = regExp.$4;
+        // Replace unit names so they don't need escaping.
+        switch (units) {
+            case '"':
+                units = 'inch';
+                break;
+            case "'":
+                units = 'feet';
+                break;
+        }
+        $element.addClass(className)
+        .data('type', 'point')
+        .data('identifier', [coordinates, units])
+        .on('mouseover', function() {
+            Bridge.call('highlight_point', coordinates, units);
+        })
+        .on('mouseout', stop);
+    }
+
+    function addVectorHighlight($element, regExp) {
+        // Add highlight feature to Vector3d or Vector3d string
+        var coordinates = [parseFloat(regExp.$1), parseFloat(regExp.$2), parseFloat(regExp.$3)];
+        $element.addClass(className)
+        .data('type', 'vector')
+        .data('identifier', coordinates)
+        .on('mouseover', function() {
+            Bridge.call('highlight_vector', coordinates);
+        })
+        .on('mouseout', stop);
+    }
+
+    function addColorHighlight($element, regExp) {
+        // Add highlight feature to Color (only HTML/JS) 
+        // when color has been printed as numerical color values.
+        addColorHighlightToHTMLElement($element, 'rgb(' + regExp.$1 + ')');
+    }
+
+    function addColorInspectHighlight($element, regExp) {
+        // Add highlight feature to Color (only HTML/JS)
+        // when color has been printed as Ruby object with id.
+        var idString = regExp.$1;
+        Bridge.get('get_color', idString).then(function (colorArray) {
+          addColorHighlightToHTMLElement($element, 'rgb(' + colorArray.slice(0, 3).join(',') + ')');
+        }, function () {
+            // If the entity isn't valid (deleted or GC), remove the highlight feature.
+            $element.removeClass(className);
+            $element.off('mouseover');
+            $element.off('mouseout');
+        });
+    }
+
+    function addColorHighlightToHTMLElement($element, colorString) {
+        $element.addClass(className)
+        .on('mouseover', function() {
+            $(this).css('background-color', colorString);
+        }).on('mouseout', function() {
+            $(this).css('background-color', 'transparent');
         });
     }
 

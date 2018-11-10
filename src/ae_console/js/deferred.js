12a04ce20cb7568a6c572b5e6d0e9770bf4fc4f8
Aerilius <aerilius@gmail.com>
2018-11-10 01:27:11 +0100

    Fix deferred.js

--- src/ae_console/js/deferred.js
+++ src/ae_console/js/deferred.js
@@ -1,17 +1,21 @@
-define(['es6-promise'], function (PromiseImplementation) {
+define(['polyfills/es6-promise'], function (PromiseImplementation) {
     PromiseImplementation.polyfill();
 
-    var resolver, rejecter;
-    this.promise = new Promise(function (_resolver, _rejecter) {
-        resolver = _resolver;
-        rejecter = _rejecter;
-    });
+    function Deferred () {
+        var resolver, rejecter;
+        this.promise = new Promise(function (_resolver, _rejecter) {
+            resolver = _resolver;
+            rejecter = _rejecter;
+        });
 
-    this.resolve = function () {
-        resolver.apply(undefined, arguments);
-    };
+        this.resolve = function () {
+            resolver.apply(undefined, arguments);
+        };
 
-    this.reject = function () {
-        rejecter.apply(undefined, arguments);
-    };
+        this.reject = function () {
+            rejecter.apply(undefined, arguments);
+        };
+    }
+    
+    return Deferred;
 });

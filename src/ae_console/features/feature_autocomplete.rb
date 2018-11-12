a9c65bce34073c5cd7f8144c8f5442f0c2de819d
Aerilius <aerilius@gmail.com>
2018-11-12 23:09:43 +0100

    Remove common prefix because it is little helpful and completions list does not stay open

--- src/ae_console/features/feature_autocomplete.rb
+++ src/ae_console/features/feature_autocomplete.rb
@@ -27,19 +27,6 @@ module AE
         completions = Autocompleter.complete_filepath(prefix)
         # Get the index of the last path separator so that we can drop the path from the displayed caption in the autocompletion list.
         directory_prefix_length = prefix.rindex('/') || -1
-        # Determine the longest prefix string that is common in all completions.
-        # Create a completion entry and put it at the top of the list.
-        longest_common_prefix = find_longest_common_prefix(completions)
-        if !completions.include?(longest_common_prefix)
-          longest_common_prefix = {
-            :caption => longest_common_prefix[directory_prefix_length+1..-1],
-            :value   => longest_common_prefix,
-            :meta    => :filepath,
-            :score   => 1000
-          }
-        else
-          longest_common_prefix = nil
-        end
         completions = completions.sort.each_with_index.map{ |filepath, index|
           {
             # Drop a directory path if contained within the prefix to make the displayed caption fit into available space.
@@ -50,25 +37,10 @@ module AE
             :score   => completions.length - index
           }
         }
-        completions << longest_common_prefix unless longest_common_prefix.nil?
         action_context.resolve completions
       rescue Exception => e
         ConsolePlugin.error(e)
         action_context.reject
-      end
-
-      def find_longest_common_prefix(strings)
-        return nil if strings.empty?
-        reference = strings.min_by(&:length)
-        pivot = 0
-        while pivot < reference.length
-          if strings.all?{ |string| string[pivot] == reference[pivot] }
-            pivot += 1
-          else
-            break
-          end
-        end
-        return reference[0...pivot]
       end
 
       def autocomplete_token_list(action_context, tokens, prefix, binding)

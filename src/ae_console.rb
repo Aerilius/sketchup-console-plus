be9ad1fce8abdc12cb44103dbdfbb1664b85c0b7
Aerilius <aerilius@gmail.com>
2018-12-04 00:56:14 +0100

    Bump version number

--- src/ae_console.rb
+++ src/ae_console.rb
@@ -9,7 +9,7 @@ ext = SketchupExtension.new('Ruby Console+', File.join('ae_console', 'core.rb'))
 # Attach some nice info.
 ext.creator     = 'Aerilius'
 ext.version     = AE::ConsolePlugin::VERSION
-ext.copyright   = '2012-2018 Andreas Eisenbarth'
+ext.copyright   = '2012-2019 Andreas Eisenbarth'
 ext.description = 'An alternative Ruby Console with command history and code highlighting.'
 
 # Register and load the extension on startup.
